d4ef939993061ed7278a2944cf117669b8694fc4
Simon Robidas <le6monstre@hotmail.com>
2016-11-23 17:09:39 -0500

    Création des controls.

--- ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/Form1.Designer.cs
+++ ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjetTetrisSession1Tp3
 {
-    partial class Form1
+    partial class FormPrincipal
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -29,11 +29,133 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.panelJeu = new System.Windows.Forms.Panel();
+            this.pictureBoxEntete = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelStatistique = new System.Windows.Forms.Panel();
+            this.panelProchainBloc = new System.Windows.Forms.Panel();
+            this.timerDescenteBloc = new System.Windows.Forms.Timer(this.components);
+            this.boutonPersonnaliseOption = new ProjetTetrisSession1Tp3.BoutonPersonnalise();
+            this.boutonPersonnaliseNouvellePartie = new ProjetTetrisSession1Tp3.BoutonPersonnalise();
+            this.pictureBoxSon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEntete)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelJeu
+            // 
+            this.panelJeu.BackColor = System.Drawing.SystemColors.Control;
+            this.panelJeu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelJeu.Location = new System.Drawing.Point(257, 131);
+            this.panelJeu.Name = "panelJeu";
+            this.panelJeu.Size = new System.Drawing.Size(336, 616);
+            this.panelJeu.TabIndex = 0;
+            // 
+            // pictureBoxEntete
+            // 
+            this.pictureBoxEntete.BackColor = System.Drawing.Color.SteelBlue;
+            this.pictureBoxEntete.Location = new System.Drawing.Point(15, 3);
+            this.pictureBoxEntete.Name = "pictureBoxEntete";
+            this.pictureBoxEntete.Size = new System.Drawing.Size(607, 119);
+            this.pictureBoxEntete.TabIndex = 1;
+            this.pictureBoxEntete.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureBoxSon);
+            this.panel2.Controls.Add(this.panelStatistique);
+            this.panel2.Controls.Add(this.panelProchainBloc);
+            this.panel2.Controls.Add(this.boutonPersonnaliseOption);
+            this.panel2.Controls.Add(this.boutonPersonnaliseNouvellePartie);
+            this.panel2.Location = new System.Drawing.Point(15, 131);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(199, 616);
+            this.panel2.TabIndex = 2;
+            // 
+            // panelStatistique
+            // 
+            this.panelStatistique.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panelStatistique.Location = new System.Drawing.Point(21, 398);
+            this.panelStatistique.Name = "panelStatistique";
+            this.panelStatistique.Size = new System.Drawing.Size(150, 175);
+            this.panelStatistique.TabIndex = 3;
+            // 
+            // panelProchainBloc
+            // 
+            this.panelProchainBloc.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panelProchainBloc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelProchainBloc.Location = new System.Drawing.Point(40, 132);
+            this.panelProchainBloc.Name = "panelProchainBloc";
+            this.panelProchainBloc.Size = new System.Drawing.Size(112, 112);
+            this.panelProchainBloc.TabIndex = 2;
+            // 
+            // timerDescenteBloc
+            // 
+            this.timerDescenteBloc.Enabled = true;
+            this.timerDescenteBloc.Tick += new System.EventHandler(this.timerDescenteBloc_Tick);
+            // 
+            // boutonPersonnaliseOption
+            // 
+            this.boutonPersonnaliseOption.BackColor = System.Drawing.Color.ForestGreen;
+            this.boutonPersonnaliseOption.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boutonPersonnaliseOption.Location = new System.Drawing.Point(21, 86);
+            this.boutonPersonnaliseOption.Name = "boutonPersonnaliseOption";
+            this.boutonPersonnaliseOption.Size = new System.Drawing.Size(150, 40);
+            this.boutonPersonnaliseOption.TabIndex = 1;
+            this.boutonPersonnaliseOption.Text = "Option";
+            this.boutonPersonnaliseOption.UseVisualStyleBackColor = false;
+            // 
+            // boutonPersonnaliseNouvellePartie
+            // 
+            this.boutonPersonnaliseNouvellePartie.BackColor = System.Drawing.Color.ForestGreen;
+            this.boutonPersonnaliseNouvellePartie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boutonPersonnaliseNouvellePartie.Location = new System.Drawing.Point(21, 40);
+            this.boutonPersonnaliseNouvellePartie.Name = "boutonPersonnaliseNouvellePartie";
+            this.boutonPersonnaliseNouvellePartie.Size = new System.Drawing.Size(150, 40);
+            this.boutonPersonnaliseNouvellePartie.TabIndex = 0;
+            this.boutonPersonnaliseNouvellePartie.Text = "Nouvelle Partie";
+            this.boutonPersonnaliseNouvellePartie.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxSon
+            // 
+            this.pictureBoxSon.BackColor = System.Drawing.Color.Silver;
+            this.pictureBoxSon.Location = new System.Drawing.Point(21, 579);
+            this.pictureBoxSon.Name = "pictureBoxSon";
+            this.pictureBoxSon.Size = new System.Drawing.Size(32, 32);
+            this.pictureBoxSon.TabIndex = 4;
+            this.pictureBoxSon.TabStop = false;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(634, 761);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBoxEntete);
+            this.Controls.Add(this.panelJeu);
+            this.DoubleBuffered = true;
+            this.Name = "FormPrincipal";
+            this.Text = "Tetris ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormPrincipal_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEntete)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSon)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelJeu;
+        private System.Windows.Forms.PictureBox pictureBoxEntete;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelProchainBloc;
+        private System.Windows.Forms.Panel panelStatistique;
+        private System.Windows.Forms.Timer timerDescenteBloc;
+        private BoutonPersonnalise boutonPersonnaliseOption;
+        private BoutonPersonnalise boutonPersonnaliseNouvellePartie;
+        private System.Windows.Forms.PictureBox pictureBoxSon;
     }
 }
 
--- ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/Form1.cs
+++ ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/Form1.cs
@@ -10,11 +10,90 @@ using System.Windows.Forms;
 
 namespace ProjetTetrisSession1Tp3
 {
-    public partial class Form1 : Form
+    public partial class FormPrincipal : Form
     {
-        public Form1()
+        SolidBrush couleur1JeuArrierePlan = new SolidBrush(Color.Beige);
+        SolidBrush couleur2JeuArrierePlan = new SolidBrush(Color.DarkGray);
+        int grosseurDesBlocs = 28;
+        Keys keysBougerADroite = Keys.Right;
+        Keys keysBougerAGauche = Keys.Left;
+        Keys keysTournerSensAntihoraire = Keys.X;
+        Keys keysTournerSensHoraire = Keys.Z;
+        public FormPrincipal()
         {
             InitializeComponent();
+            panelJeu.Focus();
+        }
+        void DessinerJeu()
+        {
+            Graphics graphicsPanelJeu = panelJeu.CreateGraphics();
+            Bitmap imageJeu = new Bitmap(panelJeu.Size.Width, panelJeu.Size.Height);
+            Graphics graphicsImageJeu = Graphics.FromImage(imageJeu);
+            SolidBrush solidBrushUtilise = couleur1JeuArrierePlan;
+            //Faire le dessinage du jeu ici-----------
+            for (int i = 0; i < 22; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    graphicsImageJeu.FillRectangle(solidBrushUtilise, grosseurDesBlocs * j, grosseurDesBlocs * i, grosseurDesBlocs, grosseurDesBlocs);
+                    solidBrushUtilise = solidBrushUtilise == couleur1JeuArrierePlan ? couleur2JeuArrierePlan : couleur1JeuArrierePlan;
+                }
+            }
+            //-----------------------------------------
+            graphicsPanelJeu.DrawImage(imageJeu, 0, 0);
+            graphicsPanelJeu.Dispose();
+            imageJeu.Dispose();
+            graphicsImageJeu.Dispose();
+        }
+
+        private void timerDescenteBloc_Tick(object sender, EventArgs e)
+        {
+            //Faire les opérations de descente ici.
+            DessinerJeu();
+        }
+
+        private void FormPrincipal_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == keysBougerADroite)
+            {
+                //Bouger à droite
+            }
+            else if (e.KeyCode == keysBougerAGauche)
+            {
+                //Bouger à gauche
+            }
+            else if (e.KeyCode == keysTournerSensAntihoraire)
+            {
+                //Tourner antihoraire
+            }
+            else if(e.KeyCode == keysTournerSensHoraire)
+            {
+                //Tourner horaire
+            }
+        }
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (keyData == keysBougerADroite)
+            {
+                //Bouger à droite
+                return true;
+            }
+            else if (keyData == keysBougerAGauche)
+            {
+                //Bouger à gauche
+                return true;
+            }
+            else if (keyData == keysTournerSensAntihoraire)
+            {
+                //Tourner antihoraire
+                return true;
+            }
+            else if (keyData == keysTournerSensHoraire)
+            {
+                //Tourner horaire
+                return true;
+            }
+            return base.ProcessDialogKey(keyData);
         }
     }
 }
