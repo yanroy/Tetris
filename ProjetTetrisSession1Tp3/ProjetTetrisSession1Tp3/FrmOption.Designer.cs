f973c1a4b45504afeaf09ff1a03303fc72a64de9
Yannick Roy Fontaine <yannick2410@hotmail.com>
2016-11-25 10:31:28 -0500

    option form

--- ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/FrmOption.Designer.cs
+++ ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/FrmOption.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmOption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 218);
+            this.Name = "FrmOption";
             this.Text = "FrmOption";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
