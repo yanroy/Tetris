67532b18c5e92e235da729c0cc4d40f2298373e3
Yannick Roy Fontaine <yannick2410@hotmail.com>
2016-11-29 11:31:23 -0500

    Master. Master. Master of puppets!

---
+++ ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/FinDePartie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetTetrisSession1Tp3
+{
+    public partial class FinDePartie : Form
+    {
+        public bool test = true;
+        public int compteurDeCarre = 0;
+        public int compteurDeLigne = 0;
+        public FinDePartie()
+        {
+            InitializeComponent();
+        }
+    }
+}
