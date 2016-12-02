16073dd0f4ee72198845d977e8c35c8f4e5d0c09
Simon Robidas <le6monstre@hotmail.com>
2016-12-02 08:07:04 -0500

    Adding score

---
+++ ProjetTetrisSession1Tp3/ProjetTetrisSession1Tp3/ScoreThread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjetTetrisSession1Tp3
+{
+    //Simon
+    public class ScoreThread
+    {
+        int posScore;
+        FormPrincipal frmPrincipal;
+        public ScoreThread(int posScore, FormPrincipal frmPrincipal)
+        {
+            this.posScore = posScore;
+            this.frmPrincipal = frmPrincipal;
+        }
+        public void ScoreTimer()
+        {
+            Thread.Sleep(4000);
+            frmPrincipal.scoreTemporaire[posScore] = 0;
+        }
+    }
+}
